c202b81f0edcf14359fd4e63ac84d5f99a9b76b1
bilgeyillmaz <96742976+bilgeyillmaz@users.noreply.github.com>
2023-02-12 14:29:52 +0300

    Add project files.

---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Adapters;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //BaseCustomerManager customerManager = new NeroCustomerManager();
+            //customerManager.Save(new Customer { DateOfBirth= new System.DateTime(1993,9,25), FirstName= "Bilge", LastName="Yılmaz", NationalityId="***********});
+
+            BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
+            customerManager.Save(new Customer { DateOfBirth= new System.DateTime(1993,9,25), FirstName= "Bilge", LastName="Yılmaz", NationalityId="***********"});
+            Console.ReadLine();
+        }
+    }
+}
